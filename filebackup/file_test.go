c6ca721ab134d2f8bef641fcb6c2f4f79e387c78
wayne <1014271609@qq.com>
2019-01-29 22:16:35 +0800

    update

--- filebackup/file_test.go
+++ filebackup/file_test.go
@@ -5,12 +5,24 @@ import (
 	"testing"
 )
 
+const rootPath  = "/Users/liuweipeng/Desktop/document/lucene"
+
+func Test_GetFileList(t *testing.T)  {
+	fileList := &pb.FileList{}
+	GetFileList(rootPath,fileList)
+	if len(fileList.File) == 0 {
+		t.Error("read fileList wrong!")
+	} else {
+		t.Log("fileList size is " , len(fileList.File))
+	}
+}
+
 func Test_BackupFileBinary(t *testing.T)  {
 	fileList := &pb.FileList{}
-	path := "C:\\Users\\liu.wp\\go\\src\\github.com\\Wayne-Liu\\hello-grpc\\model\\gen\\"
-	GetFileList(path, "",fileList)
+	//path := "C:\\Users\\liu.wp\\go\\src\\github.com\\Wayne-Liu\\hello-grpc\\model\\gen\\"
+	GetFileList(rootPath, fileList)
 
-	fileBinary := BackupFileBinary(fileList,path)
+	fileBinary := BackupFileBinary(fileList,rootPath)
 
 	if len(fileBinary.Files) >1 {
 		t.Log("读取列表数为:",len(fileBinary.Files))
@@ -21,10 +33,10 @@ func Test_BackupFileBinary(t *testing.T)  {
 
 func Test_WriteFile(t *testing.T)  {
 	fileList := &pb.FileList{}
-	path := "C:\\Users\\liu.wp\\go\\src\\github.com\\Wayne-Liu\\hello-grpc\\model\\gen\\"
-	GetFileList(path, "",fileList)
+	//path := "C:\\Users\\liu.wp\\go\\src\\github.com\\Wayne-Liu\\hello-grpc\\model\\gen\\"
+	GetFileList(rootPath, fileList)
 
-	fileBinary := BackupFileBinary(fileList,path)
+	fileBinary := BackupFileBinary(fileList,rootPath)
 	t.Log("读取列表数为:",len(fileBinary.Files))
 	basePath := "E:\\backup\\model\\"
 
